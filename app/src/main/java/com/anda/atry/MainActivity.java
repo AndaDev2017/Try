14742116ef805f03550e4ea88005881c4ab1779a
moyuwan <501824636@qq.com>
2017-10-30 16:04:07 +0800

    调整

--- app/src/main/java/com/anda/atry/MainActivity.java
+++ app/src/main/java/com/anda/atry/MainActivity.java
@@ -3,11 +3,14 @@ package com.anda.atry;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import anda.comm.TryUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TryUtils.test();
     }
 }
